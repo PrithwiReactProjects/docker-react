ca3b7cb0da422e684e665e16967535b9ad7784b6
PrithwiReactProjects <jduck399@gmail.com>
2023-06-09 03:37:54 +0530

    Update ProtectedPage.jsx

--- src/LoginPage/ProtectedPage.jsx
+++ src/LoginPage/ProtectedPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProtectedPage = ({ authenticated }) => {
 
 
-  return <div>Welcome to the Voipxp!</div>;
+  return <div>Welcome to the MYPAGE!</div>;
 };
 
 export default ProtectedPage;
